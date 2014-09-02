d90d9f488bc2eb04eb1ce2318095a98a9e32aba5
marcoaam <marco.army@gmail.com>
2014-09-02 21:47:42 +0100

    fixed a fed indentation issues

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,40 +2,40 @@ require_relative 'weather'
 
 class Airport
 
-	include Weather
+  include Weather
 
-	attr_reader :hangar, :capacity
+  attr_reader :hangar, :capacity
 
-	DEFAULT_CAPACITY = 20
+  DEFAULT_CAPACITY = 20
 
-	def initialize(options = {})
-		@capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
-		@hangar ||= []
-	end
+  def initialize(options = {})
+    @capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
+    @hangar ||= []
+  end
 
-	def has_planes?
-		hangar.any?
-	end
+  def has_planes?
+    hangar.any?
+  end
 
-	def release(plane)
-		hangar.delete(plane)
-	end
+  def release(plane)
+    hangar.delete(plane)
+  end
 
-	def receive(plane)
-		if full?
-			take_of_all_planes
-			puts 'Airport full, All planes trying to take of of weather is not stormy'
-		else
-			hangar << plane
-		end
-	end
+  def receive(plane)
+    if full?
+      take_of_all_planes
+      puts 'Airport full, All planes trying to take of of weather is not stormy'
+    else
+      hangar << plane
+    end
+  end
 
-	def full?
-		hangar.count >= capacity
-	end
+  def full?
+    hangar.count >= capacity
+  end
 
-	def take_of_all_planes
-		hangar.each { |plane| plane.take_of_from(self) }
-	end
+  def take_of_all_planes
+    hangar.each { |plane| plane.take_of_from(self) }
+  end
 
 end
